58a1a0f7020a94af54c51944186014f03362749d
Hanna Babai <kot.vmeshke.4@gmail.com>
2023-11-24 19:26:05 +0100

    refactor: video link component

--- src/components/VideoLink/VideoLink.jsx
+++ src/components/VideoLink/VideoLink.jsx
@@ -1,30 +1,12 @@
 import './VideoLink.scss';
 
-const VideoLink = ({ id, title, description, onClick, number, tgId }) => {
-
-  const sendLessonId = async (lessonId) => {
-    const formData = new FormData();
-    formData.append('lesson_id', lessonId);
-    formData.append('tgid', tgId);
-
-    const response = await fetch(
-      'https://hmns.in/prodano/public/view-counter',
-      {
-        method: 'POST',
-        body: formData,
-      }
-    );
-
-    const data = await response.text();
-    console.log(data);
-  }
+const VideoLink = ({ id, title, description, onClick, tgId }) => {
 
   return (
     <div
       className="video-link"
       onClick={() => {
         onClick(id);
-        sendLessonId(number);
       }}>
       <div className="video-link__title">{title}</div>
       <div className="video-link__descr">{description}</div>
